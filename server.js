5c46c67e393f1c9c7b803642d86e0f4a31f2b9d8
Marc D Wagner <marc.wagner@yahoo.com>
2022-10-26 18:16:04 -0700

    server runs on local host with replies in terminal and on site

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ console.log('First server');
 
 const express = require('express');
 require('dotenv').config();
-let data = require('./data/folder.json');
+let data = require('./data/weather.json');
 const cors = require('cors');
 const axios = require('axios');
 // cors is middleware to share resources across the internet
@@ -26,50 +26,40 @@ const PORT = process.env.PORT || 3002;
 
 // base endpoint
 app.get('/', (request, response) => {
-  console.log('shows up in termina');
+  console.log('Shows up in terminal');
   response.status(200).send('Welome to my server');
 });
 
-app.get('/hello', (request, response) => {
-  console.log(request.query);
-  let firstName = request.query.firstName;
-  let lastName = request.query.lastName;
-  response.status(200).send(`Hello ${firstName} ${lastName}, welcome to my server.`);
-});
+// app.get('/hello', (request, response) => {
+//   console.log(request.query);
+//   let firstName = request.query.firstName;
+//   let lastName = request.query.lastName;
+//   response.status(200).send(`Hello ${firstName} ${lastName}, welcome to my server.`);
+// });
 // todo:  build /weather route and send groomed json data - arr of 3 days of weather {date, description} - to front end
 // front-end axio.get to: http://localhost:3001/weather?searchQuery=value&lat=anothervalue&long=anothervaule
 
-// app.get('/weather', (request, response, next)) => {
-// let cityName = request.query.searchQuery;
-// let lat = request.query.lat;
-// let long = request.query.lon;
-// try {
-// let cityData = data.find(city => city.city_name === cityName);
-// let groomedData = cityData.data.map(day => new Forecast(day));
-// response.status(200).send(groomedData);
+app.get('/weather', (request, response, next) => {
+  let cityName = request.query.searchQuery;
+  let lat = request.query.lat;
+  let long = request.query.lon;
+  try {
+    let cityData = data.find(city => city.city_name === cityName);
+    let groomedData = cityData.data.map(day => new Forecast(day));
+    response.status(200).send(groomedData);
 
-// } catch (error) {
-// next(error);
-// }
-// });
+  } catch (error) {
+    next(error);
+  }
+});
 
-// app.get('/folder', (request, response, next) => {
-//   try {
-//     let value = request.query.value;
-//     let dataToGroom = data.find(element => element.value === value);
-//     let dataToSend = new Forecast(dataToGroom);
-//     response.status(200).send();
-//   } catch (error) {
-//     next(error);
-//   }
-// });
 
-// class Forecast {
-//   constructor(dayObj) {
-//     this.date = dayObj.datetime;
-//     this.description = dayObj.weather.description;
-//   }
-// }
+class Forecast {
+  constructor(dayObj) {
+    this.date = dayObj.datetime;
+    this.description = dayObj.weather.description;
+  }
+}
 
 // Catch-all
 
@@ -86,4 +76,3 @@ app.use((error, request, response, next) => {
 // ***** SERVER START *****
 
 app.listen(PORT, () => console.log(`Up and running on port ${PORT}`));
-
